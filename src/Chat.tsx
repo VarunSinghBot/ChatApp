8a4406b96b114d0b4b16e985af3953cf781ef846
Varun <varunsinghbot@gmail.com>
2025-05-20 03:11:36 +0530

    updated a bit ui

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -132,40 +132,62 @@ const Chat = () => {
           WELCOME TO CHAT APP ~ Developed By ~ Varun
         </div>
         <div className='h-full w-fit flex items-center justify-center '>
-          <div onClick={copyToClipboard} className='h-fit w-fit p-[6px] cursor-pointer ml-4 flex items-center justify-center rounded-md border border-solid border-black text-black bg-white'>
+          
+          <button onClick={copyToClipboard} className='mr-4 p-1 pl-2 pr-2 flex flex-row items-center justify-center rounded-md border border-solid border-black text-black bg-white'>
+            <div className='h-[22px] w-[22px] mr-4'>
             <img src="/Copy.svg" alt="Copy" className='h-[20px] w-[20px]'/>
           </div>
-          <button onClick={copyToClipboard} className='mr-4 p-1 pl-2 pr-2 rounded-md border border-solid border-black text-black bg-white'>
             Room ID: {roomId}
           </button>
         </div>
         
       </header>
 
-      <div ref={messageRef} className='min-h-[35%] max-h-[38rem] h-[38rem] w-[calc(50%+60px)] min-w-[360px] p-[8px] border border-solid mb-[20px] border-[rgba(255,255,255,0.25)] bg-[rgba(255,255,255,0.025)] rounded-lg hover:border-[rgba(255,255,255,0.35)] overflow-y-scroll scrollbar-hide '>
-        {messages.map((msg, idx)=>{
-          // {if(!msg) return ;}
-          return (<>
-          <div key={idx} className={`flex ${msg.type === "system" ? "justify-center" : msg.payload.name === username ? "justify-end" : "justify-start" } mb-2`}>
-            <div className={`max-w-[75%] p-2 rounded-sm ${msg.type === "system" ? "bg-yellow-600 text-white" :msg.payload.name === username ? "bg-blue-500 text-white" : "bg-gray-100 text-black"}`}>
-              <strong>{ msg.type === "chat" ? msg.payload.name === username ? "" : `${msg.payload.name}:` : ""} </strong> {msg.payload.message}
+      <main className='h-auto w-[calc(50%+60px)] min-w-[360px] flex items-center justify-center flex-col border border-solid border-[rgba(255,255,255,0.25)] hover:border-[rgba(255,255,255,0.35)] bg-[rgba(255,255,255,0.025)] rounded-lg'>
+        <div ref={messageRef} className='min-h-[35%] max-h-[38rem] h-[38rem] w-full p-[8px] mb-[20px] bg-transparent rounded-lg overflow-y-scroll scrollbar-hide '>
+          {messages.map((msg, idx)=>{
+            // {if(!msg) return ;}
+            return (<>
+            <div key={idx} className={`flex ${msg.type === "system" ? "justify-center" : msg.payload.name === username ? "justify-end" : "justify-start" } mb-2`}>
+              <div className={`max-w-[75%] p-2 rounded-sm ${msg.type === "system" ? "text-[12px] text-white/50" :msg.payload.name === username ? "bg-blue-500 text-white" : "bg-gray-100 text-black"}`}>
+                <strong>{ msg.type === "chat" ? msg.payload.name === username ? "" : `${msg.payload.name}:` : ""} </strong> {msg.payload.message}
+              </div>
             </div>
-          </div>
-        </>)})}
-            <div className='h-[60px] w-full bg-transparent text-black'></div>
-      </div>
-      <div className='h-auto w-[100%] flex items-center justify-center'> 
-        <input type="text" name="msg" onKeyDown={(e) => {if (e.key === "Enter") {sendMsg();}}} id="msgInputBox" ref={inputRef}  placeholder='Type ur msg here ...' className='h-[44px] w-[50%] min-w-[316px] text-[18px] px-2 rounded-lg border border-solid border-[rgba(255,255,255,0.25)] bg-[rgba(255,255,255,0.05)] hover:border-[rgba(255,255,255,0.35)]'/>
+          </>)})}
+              <div className='h-[60px] w-full bg-transparent text-black'></div>
+        </div>
 
-        <button onClick={()=>{
-          // sendMessage();
-          sendMsg();
-        }} className='h-[44px] w-[44px] bg-white hover:bg-[rgba(255,255,255,0.9)] text-[black] ml-4 rounded-lg'> 
-          <span className='h-full w-full flex items-center justify-center'>
-            <img src="/Send.svg" alt="Send" />
-          </span> 
-        </button> 
-      </div>
+        {/* <div className='h-[1px] w-full bg-[rgba(255,255,255,0.25)] mb-2'/> */}
+
+        {/* Input Box */}
+        <div className='h-auto w-[100%] flex items-center justify-center border-t border-solid border-[rgba(255,255,255,0.25)] bg-[rgba(255,255,255,0.05)] hover:border-[rgba(255,255,255,0.35)]'>
+          <input 
+            type="text" 
+            autoComplete="off" 
+            name="msg" 
+            onKeyDown={(e) => {
+              if (e.key === "Enter"){
+                sendMsg();
+                }
+              }
+            } id="msgInputBox" 
+            ref={inputRef}  
+            placeholder='Type ur msg here ...' 
+            className='h-[44px] w-[95%] min-w-[316px] text-[18px] px-2 rounded-lg bg-transparent outline-none focus:outline-none focus:ring-0'
+          />
+
+          <button onClick={()=>{
+            // sendMessage();
+            sendMsg();
+          }} className='h-[36px] w-[120px] bg-white hover:bg-[rgba(255,255,255,0.9)] text-[black] rounded-lg mr-1'> 
+            <span className='h-full w-full flex items-center justify-center'>
+              SEND
+              <img src="/Send.svg" alt="Send" className='w-auto h-[50%] ml-2'/>
+            </span> 
+          </button> 
+        </div>
+      </main>
+
       {!isConnected && <p className="text-red-500">Disconnected. Trying to reconnect...</p>}
       {isError && <p className="text-red-500">There was an error with the WebSocket connection.</p>}
       </div>
